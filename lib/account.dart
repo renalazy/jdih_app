c8832a58a5b5b2fe493d6b404f7605da871c96f1
renaldi <renalazyy@gmail.com>
2019-11-20 09:17:24 +0700

    first commit

---
+++ lib/account.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(AccountApp());
+  });
+}
+
+class AccountApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Account',
+      home: AccountPage(),
+    );
+  }
+}
+
+class AccountPage extends StatefulWidget {
+  @override
+  _AccountPageState createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    final makeBottom = Container(
+      height: 55.0,
+      child: BottomAppBar(
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              color: Colors.black,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.bookmark),
+              color: Colors.black,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.library_books),
+              color: Colors.black,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.person),
+              color: Colors.black,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+
+    final bodyContent = SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 40.0, horizontal: 40.0),
+            child: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('images/google-logo.png'),
+                backgroundColor: Colors.transparent,
+              ),
+              title: Text(
+                'Rosihan Andin Pambudi',
+                style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                'me@rosihan.my.id',
+                style: TextStyle(fontSize: 12.0),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20.0),
+            child: Card(
+              child: ListTile(
+                title: Text(
+                  'Profile',
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade600),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+          ),
+          Container(
+            child: Card(
+              child: ListTile(
+                title: Text(
+                  'Panduan Pengguna',
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade600),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+          ),
+          Container(
+            child: Card(
+              child: ListTile(
+                title: Text(
+                  'Frequently Asked Question (FAQ)',
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade600),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+          ),
+          Container(
+            child: Card(
+              child: ListTile(
+                title: Text(
+                  'Hubungi Kami',
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade600),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+          ),
+          Container(
+            child: Card(
+              child: ListTile(
+                title: Text(
+                  'Kunjungi Website',
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade600),
+                ),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+
+    return Scaffold(
+      body: bodyContent,
+      bottomNavigationBar: makeBottom,
+    );
+  }
+}
